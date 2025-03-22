9b86b8bf199f8404b0a32929a4fb7945797955e2
number 1 <number1@gmail.com>
2025-03-22 21:05:33 +0200

    first commit

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Post from "./Post";
-import { prisma } from "@/prisma";
 import { auth } from "@clerk/nextjs/server";
-
 const Feed = async ({ userProfileId }: { userProfileId: string }) => {
   const { userId } = await auth();
   if (!userId) return;
